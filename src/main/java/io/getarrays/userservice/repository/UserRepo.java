c9f561356c1115115233f3f6e2041aabcb842072
MD. ARIFUL ISLAM <tonu2050@gmail.com>
2021-12-02 01:53:52 +0600

    First Commit

---
+++ src/main/java/io/getarrays/userservice/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package io.getarrays.userservice.repository;
+
+import io.getarrays.userservice.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+}
